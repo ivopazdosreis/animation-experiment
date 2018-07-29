f2f402f4fb8f2c4f1e899e9aee44679f5c3abb38
Ivo Reis <41804842+ivopazdosreis@users.noreply.github.com>
2018-07-29 14:12:26 +0100

    initial commit

---
+++ Assets/_Scripts/SoldierScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoldierScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		transform.GetComponent<Animation>().Play ("death");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
